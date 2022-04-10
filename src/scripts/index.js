d4c0e5b41c875b467d9fbcb2a4239652a767262e
Sergey-Shar <sergeihargithub@gmail.com>
2022-04-10 19:20:58 +0300

    Update index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -39,27 +39,6 @@ function app() {
 
 
 
-// const SERVER_DATA = "https://6173b59d110a7400172230d3.mockapi.io/sneakers/"
 
-//     async function first (id){
-//         let result = await fetch(SERVER_DATA + id)
-//         return result.json()
-//     }
-//     async function second (id){
-//         let result = await fetch(SERVER_DATA + id)
-//         return result.json()
-//     }
-//     async function third (id) {
-//         let result = await fetch(SERVER_DATA + id)
-//         return result.json()
-//     }
-
-// async function start (){
-//  third(3)
-//  first(1)
-//  second(2)
-// }
-
-// start()
 
 
